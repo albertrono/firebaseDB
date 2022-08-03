e5973d88c78c79419d9026dc97c5ceb7fd1d3c26
albertrono <alberitorono@gmail.com>
2022-08-03 11:20:37 +0300

    initial commit

---
+++ app/src/main/java/com/example/firebase9/Rate.kt
@@ -0,0 +1,5 @@
+package com.example.firebase9
+
+class Rate (val id: String,val name:String, val rating:Int){
+    constructor():this("","",0)
+}
---
+++ app/src/main/java/com/example/firebase9/RateAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.firebase9
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class RateAdapter (val mCtxt: Context, val layoutResId: Int, val RateList:List<Rate>)
+    : ArrayAdapter<Rate>(mCtxt,layoutResId,RateList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val layoutInflater: LayoutInflater = LayoutInflater.from(mCtxt);
+        val view: View = layoutInflater.inflate(layoutResId, null);
+
+        val txt_View = view.findViewById<TextView>(R.id.txt_View);
+
+        val rate = RateList[position];
+        txt_View.text = rate.name;
+        return view;
+    }
+}
